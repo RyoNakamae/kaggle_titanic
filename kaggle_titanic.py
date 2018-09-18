bb4c1020b86c9f189cbc1ecbd883a2fb0dcbd650
Ryo Nakamae <ryo.nakamae@gmail.com>
2018-09-18 18:13:19 +0900

    いろいろなモデルを追加

--- kaggle_titanic.py
+++ kaggle_titanic.py
@@ -3,10 +3,20 @@
 # Python3
 
 import pprint
+import datetime 
 
 import pandas as pd
-from sklearn import tree
 import numpy as np
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 csv_train = r'C:\PJ\20180917_kaggle\kaggle_titanic\files\train.csv'
@@ -86,10 +96,7 @@ def output_file(prediction, df_test, filename):
     df_solution_tree.to_csv(filename, index_label = ["PassengerId"])
     pass
 
-if __name__ == '__main__':
-
-    print("start")
-
+def get_data():
     # ファイル読み込み
     df_train = pd.read_csv(csv_train)
     df_test = pd.read_csv(csv_test)
@@ -97,54 +104,157 @@ if __name__ == '__main__':
     # データの確認
     # read_datasets(df_train, df_test)
 
-    # pre_processing
+    return df_train, df_test
+
+def evaluation(model, features, target):
+    """
+    とりあえず結果を評価してみる
+    """
+    evaluation_score = round(model.score(features , target) * 100, 2)
+    print(" -- evaluation : {}".format(evaluation_score))
+
+    return evaluation_score
+
+def create_model_DecisionTree(predictor_var, response_var):
+    # 決定木の作成とアーギュメントの設定
+    max_depth = 10
+    min_samples_split = 5
+    # model_DecisionTree = tree.DecisionTreeClassifier(max_depth = max_depth, min_samples_split = min_samples_split, random_state = 1)
+    model_DecisionTree = DecisionTreeClassifier(max_depth = max_depth, min_samples_split = min_samples_split, random_state = 1)
+    model_DecisionTree = model_DecisionTree.fit(predictor_var, response_var)
+
+    return model_DecisionTree
+
+def create_model_LogisticRegression(predictor_var, response_var):
+    # ロジスティック回帰
+    model_LogisticRegression = LogisticRegression()
+    model_LogisticRegression.fit(predictor_var, response_var)
+    # Y_pred = logreg.predict(X_test)
+    return model_LogisticRegression
+
+def create_model_SVM(predictor_var, response_var):
+    # Support Vector Machines
+    model_SVC = SVC()
+    model_SVC.fit(predictor_var, response_var)
+
+    return model_SVC
+
+def create_model_KNeighbors(predictor_var, response_var):
+    # k近傍法(KNN)
+    model_KNeighbors = KNeighborsClassifier()
+    model_KNeighbors.fit(predictor_var, response_var)
+
+    return model_KNeighbors
+
+def create_model_GaussianNB(predictor_var, response_var):
+    # 単純ベイズ分類器またはナイーブベイズ分類器
+    model_GaussianNB = GaussianNB()
+    model_GaussianNB.fit(predictor_var, response_var)
+
+    return model_GaussianNB
+    
+def create_model_Perceptron(predictor_var, response_var):
+    # パーセプトロン
+    model_Perceptron = Perceptron()
+    model_Perceptron.fit(predictor_var, response_var)
+
+    return model_Perceptron
+
+def create_model_RandomForest(predictor_var, response_var):
+    # Random Forest
+    model_RandomForest = RandomForestClassifier(n_estimators=100)
+    model_RandomForest.fit(predictor_var, response_var)
+
+    return model_RandomForest
+
+if __name__ == '__main__':
+
+    print("start")
+
+    # データ取得
+    df_train, df_test = get_data()
+
+    # 前処理:pre_processing
     # pd.options.mode.chained_assignment = None
-    pre_processing(df_train)
-    pre_processing(df_test)
+    for df in [df_train, df_test]:
+        pre_processing(df)
     # pd.options.mode.chained_assignment = 'warn'
 
     # 変換後データの確認
     # read_datasets(df_train, df_test)
 
 
-    # 「train」の目的変数と説明変数の値を取得
-    target = df_train["Survived"].values
-    # features_one = df_train[["Pclass", "Sex", "Age", "Fare"]].values
-    # 決定木の作成
-    # my_tree_one = tree.DecisionTreeClassifier()
-    # my_tree_one = my_tree_one.fit(features_one, target)
-    # 「test」の説明変数の値を取得
-    # test_features = df_test[["Pclass", "Sex", "Age", "Fare"]].values
-    # 「test」の説明変数を使って「my_tree_one」のモデルで予測
-    # my_prediction = my_tree_one.predict(test_features)
-
-    # 予測結果をファイルに出力
-    # output_file(my_prediction ,df_test , "my_tree_one.csv")
-
-
-
-    # 追加となった項目も含めて予測モデルその2で使う値を取り出す
-    # lst_params = ["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]
+    # 特徴量の対象とする項目を決定
     lst_params = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
-    # features_two = df_train[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
-    features_two = df_train[lst_params].values
-    # 決定木の作成とアーギュメントの設定
-    max_depth = 10
-    min_samples_split = 5
-    my_tree_two = tree.DecisionTreeClassifier(max_depth = max_depth, min_samples_split = min_samples_split, random_state = 1)
-    my_tree_two = my_tree_two.fit(features_two, target)
+    # lst_params = ["Pclass", "Sex", "Age", "Fare"]
 
-    # tsetから「その2」で使う項目の値を取り出す
-    # test_features_2 = df_test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
-    test_features_2 = df_test[lst_params].values
-    # 「その2」の決定木を使って予測
-    my_prediction_tree_two = my_tree_two.predict(test_features_2)
+
+    # 目的変数の値を取得
+    response_var = df_train["Survived"].values
+
+    # 説明変数の値を取得：追加となった項目も含めて予測モデルで使う値を抽出
+    predictor_var = df_train[lst_params].values
+    # テストからも説明変数の値を取得：実際の予測に使用するデータ
+    test_predictor_var = df_test[lst_params].values
+
+    # それぞれの結果格納用のDataFrameを用意する
+    model_index = ["model_name","score","model"]
+    df_models = pd.DataFrame(index=[], columns=model_index)
+
+    # 確認するモデルリストを作成
+    model_list = [  "DecisionTree", 
+                    "LogisticRegression",
+                    "Support Vector Machines",
+                    "k近傍法(KNN)",
+                    "Gaussian Naive Bayes",
+                    "Perceptron","Random Forest"
+                ]
+
+    for index, name in enumerate(model_list):
+        print(index, name)
+        model = None
+        # print(index)
+        if(name == "DecisionTree"):
+            # 決定木
+            model = create_model_DecisionTree(predictor_var, response_var)
+        elif(name == "LogisticRegression"):
+            # ロジスティック回帰
+            model = create_model_LogisticRegression(predictor_var, response_var)
+        elif(name == "Support Vector Machines"):
+            # Support Vector Machines
+            model = create_model_SVM(predictor_var, response_var)
+        elif(name == "k近傍法(KNN)"):
+            # k近傍法(KNN)
+            model = create_model_KNeighbors(predictor_var, response_var)
+        elif(name == "Gaussian Naive Bayes"):
+            # 単純ベイズ分類器またはナイーブベイズ分類器(Gaussian Naive Bayes)
+            model = create_model_GaussianNB(predictor_var, response_var)
+        elif(name == "Perceptron"):
+            # パーセプトロン
+            model = create_model_Perceptron(predictor_var, response_var)
+        elif(name == "Random Forest"):
+            model = create_model_RandomForest(predictor_var, response_var)
+        else:
+            print("Don't Exist Model")
+            continue
+
+        evaluation_score = evaluation(model, predictor_var, response_var)
+        df_models = df_models.append(pd.Series([name, evaluation_score, model], index=model_index),ignore_index=True)
+
+    # scoreでソート
+    df_sorted_models = df_models.sort_values(by='score', ascending=False)
+    pprint.pprint(df_sorted_models)
+
+    # 一番精度が高そうなモデルを使って、テストデータに対して予測
+    model = df_sorted_models.iloc[0]["model"]
+    prediction = model.predict(test_predictor_var)
 
     # 予測結果をファイルに出力
-    output_file(my_prediction_tree_two ,df_test , "my_tree_two.csv")
+    output_file(prediction ,df_test , "prediction_{0:%Y%m%d%H%M%S}.csv".format(datetime.datetime.now()))
 
     print("end")
-    pass
+
+
 
 
 
